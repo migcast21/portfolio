4aaadf24aceefc8e0cb3cd0dcd3420b57f0e2651
Miguel Castaneda <miguelcastaneda@Miguels-MBP-9.lan>
2022-07-28 15:41:01 -0700

    home, bio, carousel

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 $(() => {
 
-
+//Hover to reveal text
 $('.name').hover(function(){
      $("p").show(7000);
       });
 
+//Carousel
+      let currentImgIndex = 0
+      let numOfImages = $('.carousel-images').children().length -1
+  
+      $('.next').on('click', () => {
+          $('.carousel-images').children().eq(currentImgIndex).css('display',' none')
+  
+          if(currentImgIndex < numOfImages) {
+              currentImgIndex ++
+             } else {
+              currentImgIndex = 0
+             }
+  
+          $('.carousel-images').children().eq(currentImgIndex).css('display',' block')
+      })
+  
+      $('.prev').on('click', () => {
+          $('.carousel-images').children().eq(currentImgIndex).css('display',' none')
+  
+          if(currentImgIndex > 0) {
+              currentImgIndex --
+             } else {
+              currentImgIndex = numOfImages
+             }
+  
+          $('.carousel-images').children().eq(currentImgIndex).css('display',' block')
+      })
 })
